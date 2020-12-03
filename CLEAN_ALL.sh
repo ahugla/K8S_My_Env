82f66e2357599736d80bce3bd68b3be117555b70
ahugla <ahugla@vmware.com>
2020-12-03 11:22:06 +0100

    Update CLEAN_ALL.sh

--- CLEAN_ALL.sh
+++ CLEAN_ALL.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+# DELETE ALL SERVICES IN NAMESPACE DEFAULT
+svcToDelete=`kubectl get service -n default | awk {'print $1'}  | sed '/NAME/d' | sed '/kubernetes/d'`
+if [ "$svcToDelete" == "" ]
+then
+  echo "Aucun Service"
+else
+  echo "Suppression des services " $svcToDelete
+  kubectl delete service $svcToDelete --namespace=default
+fi
+
 # DELETE ALL DEPLOYMENT IN NAMESPACE DEFAULT
 kubectl delete deployment --all --namespace=default
 
@@ -12,12 +23,8 @@ kubectl delete rs --all --namespace=default
 # DELETE ALL PODS IN NAMESPACE DEFAULT
 kubectl delete pod --all --namespace=default
 
-# DELETE ALL SERVICES IN NAMESPACE DEFAULT
-svcToDelete=`kubectl get service | awk {'print $1'}  | sed '/NAME/d' | sed '/kubernetes/d'`
-if [ "$svcToDelete" == "" ]
-then
-  echo "Aucun Service"
-else
-  echo "Suppression des services " $svcToDelete
-  kubectl delete service $svcToDelete --namespace=default
-fi
+
+
+
+#sleep 10      # attend 10s
+#kubectl delete pods tito-fe-rc-newversion-sb5xl   --grace-period=0 --force
